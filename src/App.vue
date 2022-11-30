a4d06ff324859b2bb2deecdfcb7c36725ba7b345
Alejandro Soto Castro <alejandrosc.eng@gmail.com>
2022-11-30 09:10:40 +0100

    Fixes

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,12 @@ function calcRank(distribution, stats) {
   if (stats.length !== 6) return;
   //my stats
   const pointValue = utils.calcPoints(stats);
-  return utils.percentRank(distribution, pointValue).toFixed(4) * 100;
+  console.log(stats, pointValue);
+  const res = {
+    points: pointValue,
+    rank: utils.percentRank(distribution, pointValue).toFixed(4) * 100,
+  };
+  return res;
 }
 export default {
   data() {
@@ -26,14 +31,12 @@ export default {
     addTodo() {
       const inputs = [
         Number(this.stat1),
-        Number(this.stat1),
-        Number(this.stat1),
-        Number(this.stat1),
-        Number(this.stat1),
-        Number(this.stat1),
+        Number(this.stat2),
+        Number(this.stat3),
+        Number(this.stat4),
+        Number(this.stat5),
+        Number(this.stat6),
       ];
-      console.log(utils.statRoll(), inputs);
-      const myStats = [15, 10, 10, 15, 10, 16];
       this.result = calcRank(distribution, inputs);
     },
   },
@@ -50,5 +53,8 @@ export default {
     <input v-model="stat6" />
     <button>Add Todo</button>
   </form>
-  <p>Your stats are better than the {{ result }}% of possible stats</p>
+  <div v-if="result">
+    <p>Your point equivalent is {{ result.points }}</p>
+    <p>Your stats are better than the {{ result.rank }}% of possible stats</p>
+  </div>
 </template>
